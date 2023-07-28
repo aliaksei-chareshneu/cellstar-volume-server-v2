ef4b190de8758b132993de5b5f35167100e274d7
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2023-07-28 11:37:37 +0300

    fix bug

--- preprocessor/cellstar_preprocessor/tests/helper_methods.py
+++ preprocessor/cellstar_preprocessor/tests/helper_methods.py
@@ -10,3 +10,46 @@ def initialize_intermediate_zarr_structure_for_tests():
         str(INTERMEDIATE_ZARR_STRUCTURE_PATH_FOR_TESTS)
     )
     root = zarr.group(store=store)
+
+    root.attrs["metadata_dict"] = {
+        "entry_id": {"source_db_name": None, "source_db_id": None},
+        "volumes": {
+            "channel_ids": [],
+            # Values of time dimension
+            "time_info": {
+                "kind": "range",
+                "start": None,
+                "end": None,
+                "units": None,
+            },
+            "volume_sampling_info": {
+                # Info about "downsampling dimension"
+                "spatial_downsampling_levels": [],
+                # the only thing with changes with SPATIAL downsampling is box!
+                "boxes": {},
+                # time -> channel_id
+                "descriptive_statistics": {},
+                "time_transformations": [],
+                "source_axes_units": None,
+            },
+            "original_axis_order": None,
+        },
+        "segmentation_lattices": {
+            "segmentation_lattice_ids": [],
+            "segmentation_sampling_info": {},
+            "channel_ids": {},
+            "time_info": {},
+        },
+        "segmentation_meshes": {
+            "mesh_component_numbers": {},
+            "detail_lvl_to_fraction": {},
+        },
+    }
+
+    root.attrs["annotations_dict"] = {
+        "entry_id": {"source_db_name": None, "source_db_id": None},
+        "segmentation_lattices": [],
+        "details": None,
+        "name": None,
+        "volume_channels_annotations": [],
+    }
