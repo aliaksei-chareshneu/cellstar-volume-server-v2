eb54ecc463084b0675e7edbbf1d8f82cafb8087c
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2023-07-29 14:00:38 +0300

    support for mesh segmentation

--- preprocessor/cellstar_preprocessor/tests/test_extract_annotations_from_sff_segmentation.py
+++ preprocessor/cellstar_preprocessor/tests/test_extract_annotations_from_sff_segmentation.py
@@ -2,14 +2,19 @@
 from cellstar_preprocessor.flows.segmentation.extract_annotations_from_sff_segmentation import extract_annotations_from_sff_segmentation
 from cellstar_preprocessor.flows.segmentation.helper_methods import extract_raw_annotations_from_sff
 from cellstar_preprocessor.flows.segmentation.sff_preprocessing import sff_preprocessing
+from cellstar_preprocessor.model.input import SegmentationPrimaryDescriptor
 from cellstar_preprocessor.model.segmentation import InternalSegmentation
 from cellstar_preprocessor.tests.helper_methods import initialize_intermediate_zarr_structure_for_tests
-from cellstar_preprocessor.tests.input_for_tests import INTERNAL_SEGMENTATION_FOR_TESTING
+from cellstar_preprocessor.tests.input_for_tests import INTERNAL_MESH_SEGMENTATION_FOR_TESTING, INTERNAL_SEGMENTATION_FOR_TESTING
 import pytest
 
+SEGMENTATIONS = [
+    INTERNAL_SEGMENTATION_FOR_TESTING,
+    INTERNAL_MESH_SEGMENTATION_FOR_TESTING
+]
 
-def test_extract_annotations_from_sff_segmentation():
-    internal_segmentation = INTERNAL_SEGMENTATION_FOR_TESTING
+@pytest.mark.parametrize("internal_segmentation", SEGMENTATIONS)
+def test_extract_annotations_from_sff_segmentation(internal_segmentation):
     initialize_intermediate_zarr_structure_for_tests()
     
     # NOTE: tests only three_d_volume segmentation, not mesh
@@ -27,7 +32,11 @@ def test_extract_annotations_from_sff_segmentation():
 
 
     for segment in r["segment_list"]:
-        lattice_id = str(segment["three_d_volume"]["lattice_id"])
+        if internal_segmentation.primary_descriptor == SegmentationPrimaryDescriptor.three_d_volume:
+            lattice_id = str(segment["three_d_volume"]["lattice_id"])
+        elif internal_segmentation.primary_descriptor == SegmentationPrimaryDescriptor.mesh_list:
+            lattice_id = '0'
+            
         our_lattice = list(filter(lambda lat: lat["lattice_id"] == lattice_id, d["segmentation_lattices"]))[0]
         our_segment_list = our_lattice["segment_list"]
         our_segment = list(filter(lambda seg: seg["id"] == segment["id"], our_segment_list))[0]
