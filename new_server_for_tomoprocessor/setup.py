09824ce92aedcb5ed5121b326495ed817459ad39
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2023-10-18 09:49:30 +0200

    make new server for tomoprocessor installable package

---
+++ new_server_for_tomoprocessor/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+  name='new_server_for_tomoprocessor',
+  version='0.1',
+  packages=setuptools.find_packages(
+    include=["new_server_for_tomoprocessor"]
+  )
+)
