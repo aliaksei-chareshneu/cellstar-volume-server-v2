b833eb950105f0a02769f54c93aaac9b214e0724
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2023-10-31 07:53:46 +0100

    fix mistakes; cell query

--- local_api_query.py
+++ local_api_query.py
@@ -3,7 +3,7 @@ import asyncio
 from enum import Enum
 import json
 from pathlib import Path
-from typing import Literal, TypedDict
+from typing import Literal, Optional, Protocol, Type, TypedDict, Union
 from attr import dataclass
 
 from cellstar_db.file_system.db import FileSystemVolumeServerDB
@@ -20,12 +20,6 @@ from cellstar_query.query import get_list_entries_keywords_query, get_list_entri
 
 DEFAULT_MAX_POINTS = 100000000
 
-# class QueryTypes(str, Enum):
-#     TODO:
-# query types as typed dicts?
-# entryDataRequired: bool => entry-id source-db arguments
-# hasSegmentation: bool => lattice-id argument
-# 
 @dataclass
 class BaseQuery:
     name: str # can be Enum
@@ -78,6 +72,129 @@ QUERY_TYPES = [
     ListEntriesQuery(name='list-entries-keyword', keywords=True)
 ]
 
+# TODO: add others
+COMPOSITE_QUERY_TYPES = ['volume-and-segmentation-cell']
+QUERY_TYPES_WITH_JSON_RESPONSE = ['annotations', 'metadata', 'list-entries', 'list-entries-keyword']
+
+@dataclass
+class QueryResponse:
+    response: Union[bytes, str]
+    # can be determined based on type of response
+    # file_writing_mode: Literal['w', 'wb']
+
+class QuerySpecificParams(TypedDict):
+    data_type: Optional[Literal['volume', 'segmentation']]
+
+# typeddict query task params
+class QueryTaskParams(TypedDict):
+    argaprse_args: argparse.Namespace
+    volume_server: VolumeServerService
+    custom_params: QuerySpecificParams
+
+# TODO: file writing mode
+class QueryTaskBase(Protocol):  
+    async def execute(self) -> None:
+        ...
+
+class QueryTask(QueryTaskBase):
+    def __init__(self, params: QueryTaskParams):
+        args, volume_server, query_specific_params = params.values()
+        self.volume_server = volume_server
+
+class EntryDataRequiredQueryTask(QueryTask):
+    def __init__(self, params: QueryTaskParams):
+        args, volume_server, query_specific_params = params.values()
+        super().__init__(params)
+        self.entry_id = args.entry_id
+        self.source_db = args.source_db
+
+class DataQueryTask(EntryDataRequiredQueryTask):
+    def __init__(self, params: QueryTaskParams):
+        args, volume_server, query_specific_params = params.values()
+        super().__init__(params)
+        self.time = args.time
+        self.channel_id = args.channel_id
+
+class VolumetricDataQueryTask(DataQueryTask):
+    def __init__(self, params: QueryTaskParams):
+        args, volume_server, query_specific_params = params.values()
+        super().__init__(params)
+        self.max_points = args.max_points
+        self.data_type = query_specific_params['data_type']
+        if self.data_type == 'segmentation':
+            self.lattice_id = args.lattice_id
+
+class BoxDataQueryTask(VolumetricDataQueryTask):
+    def __init__(self, params: QueryTaskParams):
+        args, volume_server, query_specific_params = params.values()
+        super().__init__(params)
+        self.box_coords = args.box_coords
+            
+    async def execute(self):
+        a1, a2, a3, b1, b2, b3 = self.box_coords
+        if self.data_type == 'volume':
+            response = await get_volume_box_query(
+                volume_server=self.volume_server,
+                source=self.source_db,
+                id=self.entry_id,
+                time=self.time,
+                channel_id=self.channel_id,
+                a1=a1,
+                a2=a2,
+                a3=a3,
+                b1=b1,
+                b2=b2,
+                b3=b3,
+                max_points=self.max_points
+            )
+        elif self.data_type == 'segmentation':
+            response = await get_segmentation_box_query(
+                volume_server=self.volume_server,
+                source=self.source_db,
+                id=self.entry_id,
+                time=self.time,
+                channel_id=self.channel_id,
+                lattice_id=self.lattice_id,
+                a1=a1,
+                a2=a2,
+                a3=a3,
+                b1=b1,
+                b2=b2,
+                b3=b3,
+                max_points=self.max_points
+            )
+
+        return QueryResponse(response=response)
+
+
+class CellDataQueryTask(VolumetricDataQueryTask):
+    def __init__(self, params: QueryTaskParams):
+        super().__init__(params)
+    async def execute(self):
+        if self.data_type == 'volume':
+            response = await get_volume_cell_query(
+                volume_server=self.volume_server,
+                source=self.source_db,
+                id=self.entry_id,
+                time=self.time,
+                channel_id=self.channel_id,
+                max_points=self.max_points
+            )
+        elif self.data_type == 'segmentation':
+            response = await get_segmentation_cell_query(
+                volume_server=self.volume_server,
+                source=self.source_db,
+                id=self.entry_id,
+                time=self.time,
+                channel_id=self.channel_id,
+                lattice_id=self.lattice_id,
+                max_points=self.max_points
+            )
+        
+        return QueryResponse(response=response)
+
+
+
 def _create_parsers(common_subparsers, query_types: list[BaseQuery]):
     parsers = []
     for query in query_types:
@@ -115,9 +232,7 @@ def _create_parsers(common_subparsers, query_types: list[BaseQuery]):
     # print(parsers)
     return parsers
 
-# TODO: add others
-COMPOSITE_QUERY_TYPES = ['volume-and-segmentation-cell']
-QUERY_TYPES_WITH_JSON_RESPONSE = ['annotations', 'metadata', 'list-entries', 'list-entries-keyword']
+
 
 async def _query(args):
     db = FileSystemVolumeServerDB(folder=Path(args.db_path))
@@ -125,172 +240,176 @@ async def _query(args):
     # initialize server
     VOLUME_SERVER = VolumeServerService(db)
 
-    file_writing_mode = 'wb'
+    # file_writing_mode = 'wb'
 
-    if args.query_type == 'volume-box':
-        print('volume box query')
-        # query
-        a1, a2, a3, b1, b2, b3 = args.box_coords
-        response = await get_volume_box_query(
-            volume_server=VOLUME_SERVER,
-            source=args.source_db,
-            id=args.entry_id,
-            time=args.time,
-            channel_id=args.channel_id,
-            a1=a1,
-            a2=a2,
-            a3=a3,
-            b1=b1,
-            b2=b2,
-            b3=b3,
-            max_points=args.max_points
-        )
+    # d = QueryTask(QueryTaskParams(argaprse_args=args, volume_server, query_specific_params=VOLUME_SERVER))
+    d = BoxDataQueryTask(args, data_type='volume')
+    print(d)
 
-    elif args.query_type == 'segmentation-box':
-        print('segmentation box query')
-        # query
-        a1, a2, a3, b1, b2, b3 = args.box_coords
-        response = await get_segmentation_box_query(
-            volume_server=VOLUME_SERVER,
-            segmentation=args.lattice_id,
-            source=args.source_db,
-            id=args.entry_id,
-            time=args.time,
-            channel_id=args.channel_id,
-            a1=a1,
-            a2=a2,
-            a3=a3,
-            b1=b1,
-            b2=b2,
-            b3=b3,
-            max_points=args.max_points
-        )
+    # if args.query_type == 'volume-box':
+    #     print('volume box query')
+    #     # query
+    #     a1, a2, a3, b1, b2, b3 = args.box_coords
+    #     response = await get_volume_box_query(
+    #         volume_server=VOLUME_SERVER,
+    #         source=args.source_db,
+    #         id=args.entry_id,
+    #         time=args.time,
+    #         channel_id=args.channel_id,
+    #         a1=a1,
+    #         a2=a2,
+    #         a3=a3,
+    #         b1=b1,
+    #         b2=b2,
+    #         b3=b3,
+    #         max_points=args.max_points
+    #     )
+
+    # elif args.query_type == 'segmentation-box':
+    #     print('segmentation box query')
+    #     # query
+    #     a1, a2, a3, b1, b2, b3 = args.box_coords
+    #     response = await get_segmentation_box_query(
+    #         volume_server=VOLUME_SERVER,
+    #         segmentation=args.lattice_id,
+    #         source=args.source_db,
+    #         id=args.entry_id,
+    #         time=args.time,
+    #         channel_id=args.channel_id,
+    #         a1=a1,
+    #         a2=a2,
+    #         a3=a3,
+    #         b1=b1,
+    #         b2=b2,
+    #         b3=b3,
+    #         max_points=args.max_points
+    #     )
     
-    elif args.query_type == 'segmentation-cell':
-        print('segmentation cell query')
-        response = await get_segmentation_cell_query(
-            volume_server=VOLUME_SERVER,
-            segmentation=args.lattice_id,
-            source=args.source_db,
-            id=args.entry_id,
-            time=args.time,
-            channel_id=args.channel_id,
-            max_points=args.max_points
-        )
-    elif args.query_type == 'volume-cell':
-        print('volume cell query')
-        response = await get_volume_cell_query(
-            volume_server=VOLUME_SERVER,
-            source=args.source_db,
-            id=args.entry_id,
-            time=args.time,
-            channel_id=args.channel_id,
-            max_points=args.max_points
-        )
+    # elif args.query_type == 'segmentation-cell':
+    #     print('segmentation cell query')
+    #     response = await get_segmentation_cell_query(
+    #         volume_server=VOLUME_SERVER,
+    #         segmentation=args.lattice_id,
+    #         source=args.source_db,
+    #         id=args.entry_id,
+    #         time=args.time,
+    #         channel_id=args.channel_id,
+    #         max_points=args.max_points
+    #     )
+    # elif args.query_type == 'volume-cell':
+    #     print('volume cell query')
+    #     response = await get_volume_cell_query(
+    #         volume_server=VOLUME_SERVER,
+    #         source=args.source_db,
+    #         id=args.entry_id,
+    #         time=args.time,
+    #         channel_id=args.channel_id,
+    #         max_points=args.max_points
+    #     )
 
-    elif args.query_type == 'mesh':
-        print('mesh query')
-        file_writing_mode = 'w'
-        response = await get_meshes_query(
-            volume_server=VOLUME_SERVER,
-            source=args.source_db,
-            id=args.entry_id,
-            time=args.time,
-            channel_id=args.channel_id,
-            segment_id=args.segment_id,
-            detail_lvl=args.detail_lvl
-        )
+    # elif args.query_type == 'mesh':
+    #     print('mesh query')
+    #     file_writing_mode = 'w'
+    #     response = await get_meshes_query(
+    #         volume_server=VOLUME_SERVER,
+    #         source=args.source_db,
+    #         id=args.entry_id,
+    #         time=args.time,
+    #         channel_id=args.channel_id,
+    #         segment_id=args.segment_id,
+    #         detail_lvl=args.detail_lvl
+    #     )
 
-    elif args.query_type == 'mesh-bcif':
-        print('mesh-bcif query')
-        response = await get_meshes_bcif_query(
-            volume_server=VOLUME_SERVER,
-            source=args.source_db,
-            id=args.entry_id,
-            time=args.time,
-            channel_id=args.channel_id,
-            segment_id=args.segment_id,
-            detail_lvl=args.detail_lvl
-        )
+    # elif args.query_type == 'mesh-bcif':
+    #     print('mesh-bcif query')
+    #     response = await get_meshes_bcif_query(
+    #         volume_server=VOLUME_SERVER,
+    #         source=args.source_db,
+    #         id=args.entry_id,
+    #         time=args.time,
+    #         channel_id=args.channel_id,
+    #         segment_id=args.segment_id,
+    #         detail_lvl=args.detail_lvl
+    #     )
 
-    elif args.query_type == 'metadata':
-        print('metadata query')
-        file_writing_mode = 'w'
-        response = await get_metadata_query(volume_server=VOLUME_SERVER, source=args.source_db, id=args.entry_id)
+    # elif args.query_type == 'metadata':
+    #     print('metadata query')
+    #     file_writing_mode = 'w'
+    #     response = await get_metadata_query(volume_server=VOLUME_SERVER, source=args.source_db, id=args.entry_id)
     
-    elif args.query_type == 'volume-info':
-        print('volume info query')
-        response = await get_volume_info_query(volume_server=VOLUME_SERVER, source=args.source_db, id=args.entry_id)
+    # elif args.query_type == 'volume-info':
+    #     print('volume info query')
+    #     response = await get_volume_info_query(volume_server=VOLUME_SERVER, source=args.source_db, id=args.entry_id)
 
-    elif args.query_type == 'annotations':
-        print('annotations query')
-        file_writing_mode = 'w'
-        metadata_response = await get_metadata_query(volume_server=VOLUME_SERVER, source=args.source_db, id=args.entry_id)
-        response = metadata_response['annotation']
+    # elif args.query_type == 'annotations':
+    #     print('annotations query')
+    #     file_writing_mode = 'w'
+    #     metadata_response = await get_metadata_query(volume_server=VOLUME_SERVER, source=args.source_db, id=args.entry_id)
+    #     response = metadata_response['annotation']
 
-    elif args.query_type == 'list-entries':
-        print('list_entries query')
-        file_writing_mode = 'w'
-        response = await get_list_entries_query(volume_server=VOLUME_SERVER, limit=args.limit)
+    # elif args.query_type == 'list-entries':
+    #     print('list_entries query')
+    #     file_writing_mode = 'w'
+    #     response = await get_list_entries_query(volume_server=VOLUME_SERVER, limit=args.limit)
     
-    elif args.query_type == 'list-entries-keyword':
-        print('list_entries query')
-        file_writing_mode = 'w'
-        response = await get_list_entries_keywords_query(volume_server=VOLUME_SERVER, limit=args.limit, keyword=args.keyword)
+    # elif args.query_type == 'list-entries-keyword':
+    #     print('list_entries query')
+    #     file_writing_mode = 'w'
+    #     response = await get_list_entries_keywords_query(volume_server=VOLUME_SERVER, limit=args.limit, keyword=args.keyword)
     
-    elif args.query_type == 'volume-and-segmentation-cell':
-        print('volume-and-segmentation-cell')
-        # PLAN
-        # 1. get one file
-        # 2. get another file
-        # zip?
-        # in memory?
-        # https://stackoverflow.com/questions/71251353/how-to-create-a-zip-archive-containing-multiple-files-and-subfolders-in-memory
-        # then write to zip file
-        # https://stackoverflow.com/questions/18457678/python-write-in-memory-zip-to-file
-        # write bytes to file
-        # https://stackoverflow.com/a/54464733/13136429
-        # if not, this:
-        # https://stackoverflow.com/questions/54200941/zipfile-module-for-python3-6-write-to-bytes-instead-of-files-for-odoo
-        file_writing_mode = 'wb'
-        volume_bcif_bytes = await get_volume_cell_query(
-            volume_server=VOLUME_SERVER,
-            source=args.source_db,
-            id=args.entry_id,
-            time=args.time,
-            channel_id=args.channel_id,
-            max_points=args.max_points
-        )
-        segmentation_bcif_bytes = await get_segmentation_cell_query(
-            volume_server=VOLUME_SERVER,
-            segmentation=args.lattice_id,
-            source=args.source_db,
-            id=args.entry_id,
-            time=args.time,
-            channel_id=args.channel_id,
-            max_points=args.max_points
-        )
-        response = [
-            ('volume.bcif', volume_bcif_bytes),
-            ('segmentation.bcif', segmentation_bcif_bytes)
-        ]
+    # elif args.query_type == 'volume-and-segmentation-cell':
+    #     print('volume-and-segmentation-cell')
+    #     # PLAN
+    #     # 1. get one file
+    #     # 2. get another file
+    #     # zip?
+    #     # in memory?
+    #     # https://stackoverflow.com/questions/71251353/how-to-create-a-zip-archive-containing-multiple-files-and-subfolders-in-memory
+    #     # then write to zip file
+    #     # https://stackoverflow.com/questions/18457678/python-write-in-memory-zip-to-file
+    #     # write bytes to file
+    #     # https://stackoverflow.com/a/54464733/13136429
+    #     # if not, this:
+    #     # https://stackoverflow.com/questions/54200941/zipfile-module-for-python3-6-write-to-bytes-instead-of-files-for-odoo
+    #     file_writing_mode = 'wb'
+    #     volume_bcif_bytes = await get_volume_cell_query(
+    #         volume_server=VOLUME_SERVER,
+    #         source=args.source_db,
+    #         id=args.entry_id,
+    #         time=args.time,
+    #         channel_id=args.channel_id,
+    #         max_points=args.max_points
+    #     )
+    #     segmentation_bcif_bytes = await get_segmentation_cell_query(
+    #         volume_server=VOLUME_SERVER,
+    #         segmentation=args.lattice_id,
+    #         source=args.source_db,
+    #         id=args.entry_id,
+    #         time=args.time,
+    #         channel_id=args.channel_id,
+    #         max_points=args.max_points
+    #     )
+    #     response = [
+    #         ('volume.bcif', volume_bcif_bytes),
+    #         ('segmentation.bcif', segmentation_bcif_bytes)
+    #     ]
         
 
     # write to file
 
     
-    with open(str((Path(args.out)).resolve()), file_writing_mode) as f:
-        if args.query_type in QUERY_TYPES_WITH_JSON_RESPONSE:
-            json.dump(response, f, indent=4)
-        elif args.query_type == 'mesh':
-            json_dump = json.dumps(response, 
-                       cls=_NumpyJsonEncoder)
-            json.dump(json_dump, f, indent=4)
-        elif args.query_type in COMPOSITE_QUERY_TYPES:
-            zip_data = create_in_memory_zip_from_bytes(response)
-            f.write(zip_data)
-        else: 
-            f.write(response)
+    # with open(str((Path(args.out)).resolve()), file_writing_mode) as f:
+    #     if args.query_type in QUERY_TYPES_WITH_JSON_RESPONSE:
+    #         json.dump(response, f, indent=4)
+    #     elif args.query_type == 'mesh':
+    #         json_dump = json.dumps(response, 
+    #                    cls=_NumpyJsonEncoder)
+    #         json.dump(json_dump, f, indent=4)
+    #     elif args.query_type in COMPOSITE_QUERY_TYPES:
+    #         zip_data = create_in_memory_zip_from_bytes(response)
+    #         f.write(zip_data)
+    #     else: 
+    #         f.write(response)
 
 async def main():
     # initialize dependencies
