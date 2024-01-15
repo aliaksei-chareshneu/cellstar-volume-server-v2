dd4093ef5c375a3c7961299fcc4f07605b74798f
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-01-15 10:19:20 +0300

    fix get cell test according to new data model

--- server/app/tests/get_cell.py
+++ server/app/tests/get_cell.py
@@ -4,8 +4,6 @@ import requests
 
 from server.app.tests._test_server_runner import ServerTestBase
 
-# TODO: max points seem not to work?
-
 test_configs = {
     "emdb": {
         "emd-1832": {
@@ -25,7 +23,6 @@ test_configs = {
 class FetchVolumeTest(ServerTestBase):
     def __fetch_for_test(self, db: str, entry: str, params: dict) -> str:
         r = requests.get(
-            # TODO: add test for segmentation cell
             f'{self.serverUrl()}/v2/{db}/{entry}/volume/cell/{params.get("time")}/{params.get("channel_id")}?max_points={params.get("max_points")}'
         )
         self.assertEqual(r.status_code, 200)
@@ -50,6 +47,37 @@ class FetchVolumeTest(ServerTestBase):
                         self.assertAlmostEqual(len(out_of_boundaries) / (8 * len(down_sampled)), 1, delta=0.4)
         finally:
             pass
+
+# NOTE: this test won't work for segmentation probably
+# because it contains set table which does not scale with downsampling
+# class FetchSegmentationTest(ServerTestBase):
+#     def __fetch_for_test(self, db: str, entry: str, params: dict) -> str:
+#         r = requests.get(
+#             # @app.get("/v2/{source}/{id}/segmentation/cell/{segmentation}/{time}")
+#             f'{self.serverUrl()}/v2/{db}/{entry}/segmentation/cell/{params.get("segmentation")}/{params.get("time")}?max_points={params.get("max_points")}'
+#         )
+#         self.assertEqual(r.status_code, 200)
+#         body = r.text
+#         self.assertIsNotNone(body)
+#         return body
+
+#     def test(self):
+#         try:
+#             with self.server.run_in_thread():
+#                 for db in test_configs.keys():
+#                     entries = test_configs.get(db)
+#                     for entry_id in entries.keys():
+#                         entry = entries.get(entry_id)
+
+#                         out_of_boundaries = self.__fetch_for_test(db, entry_id, entry.get("cell"))
+#                         print("[VolumeTests] Case *cell* -> received body of len : " + str(len(out_of_boundaries)))
+
+#                         down_sampled = self.__fetch_for_test(db, entry_id, entry.get("down_sampled"))
+#                         print("[VolumeTests] Case *down_sampled* -> received body of len : " + str(len(down_sampled)))
+
+#                         self.assertAlmostEqual(len(out_of_boundaries) / (8 * len(down_sampled)), 1, delta=0.4)
+#         finally:
+#             pass
 
 
 if __name__ == "__main__":
