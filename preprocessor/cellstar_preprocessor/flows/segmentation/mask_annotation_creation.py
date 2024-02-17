45e78c36022733e5a47344c46b817520d73094a2
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-02-17 11:33:15 +0300

    update mask segmentation preprocessing to support multiple lattices; update mask annotation creation according to new data model; (tested on EMD-17129 with a single mask)

--- preprocessor/cellstar_preprocessor/flows/segmentation/mask_annotation_creation.py
+++ preprocessor/cellstar_preprocessor/flows/segmentation/mask_annotation_creation.py
@@ -1,4 +1,5 @@
-from cellstar_db.models import EntryId
+from uuid import uuid4
+from cellstar_db.models import AnnotationsMetadata, DescriptionData, EntryId, SegmentAnnotationData, TargetId
 from cellstar_preprocessor.flows.common import open_zarr_structure_from_path
 from cellstar_preprocessor.flows.constants import LATTICE_SEGMENTATION_DATA_GROUPNAME
 from cellstar_preprocessor.model.segmentation import InternalSegmentation
@@ -12,7 +13,7 @@ def mask_annotation_creation(internal_segmentation: InternalSegmentation):
     root = open_zarr_structure_from_path(
         internal_segmentation.intermediate_zarr_structure_path
     )
-    d = root.attrs["annotations_dict"]
+    d: AnnotationsMetadata = root.attrs["annotations_dict"]
 
     d["entry_id"] = EntryId(
         source_db_id=internal_segmentation.entry_data.source_db_id,
@@ -24,8 +25,6 @@ def mask_annotation_creation(internal_segmentation: InternalSegmentation):
 
     
     for lattice_id, lattice_gr in root[LATTICE_SEGMENTATION_DATA_GROUPNAME].groups():
-        segmentation_lattice_info = {"lattice_id": lattice_id, "segment_list": []}
-
         # int to int dict
         value_to_segment_id_dict = internal_segmentation.value_to_segment_id_dict[int(lattice_id)]
         # TODO: check if 0
@@ -36,26 +35,40 @@ def mask_annotation_creation(internal_segmentation: InternalSegmentation):
         for index, value in enumerate(value_to_segment_id_dict.keys()):
             segment_id = value_to_segment_id_dict[value]
             if segment_id > 0:
-                segmentation_lattice_info["segment_list"].append(
-                        {
-                            "id": segment_id,
-                            "biological_annotation": {
-                                "name": f"Segment {segment_id}",
-                                "external_references": [
-                                ],
-                            },
-                            # TODO: find way to map integers to unique colors
-                            "color": [
-                                        palette[index][0],
-                                        palette[index][1],
-                                        palette[index][2],
-                                        1.0
-                                        ],
-                        }
-                    )
+                # create description
+                description_id = str(uuid4())
+                target_id: TargetId = {
+                    'segment_id': segment_id,
+                    'segmentation_id': str(lattice_id)
+                }
+                description: DescriptionData = {
+                    'id': description_id,
+                    'target_kind': "lattice",
+                    'description': None,
+                    'is_hidden': None,
+                    'metadata': None,
+                    'time': 0,
+                    'name': f"Segment {segment_id}",
+                    'external_references': [],
+                    'target_id': target_id
+                }
+                # create segment annotation
+                segment_annotation: SegmentAnnotationData = {
+                    'id': str(uuid4()),
+                    'color': [
+                                palette[index][0],
+                                palette[index][1],
+                                palette[index][2],
+                                1.0
+                            ],
+                    'segmentation_id': str(lattice_id),
+                    'segment_id': segment_id,
+                    'segment_kind': 'lattice',
+                    'time': 0
+                }
+                d['descriptions'][description_id] = description
+                d['annotations'].append(segment_annotation)
 
-        d["segmentation_lattices"].append(segmentation_lattice_info)
-    
     root.attrs["annotations_dict"] = d
     print("Annotations extracted")
     return d
