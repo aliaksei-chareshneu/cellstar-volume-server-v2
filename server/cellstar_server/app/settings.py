be889d814d3859a762d978e2b692a7893308872b
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-04-08 15:59:48 +0300

    remove outdated comments

--- server/cellstar_server/app/settings.py
+++ server/cellstar_server/app/settings.py
@@ -6,11 +6,7 @@ class _Settings(BaseSettings):
     PORT: int = 9000
     DEV_MODE: bool = False
     DB_PATH: Path = Path('preprocessor/temp/test_db')
-    
-    # GIT_TAG: str = _get_git_tag()
-    # GIT_SHA: str = _get_git_revision_hash()
     GIT_TAG: str = ''
     GIT_SHA: str = ''
-
 
 settings = _Settings()
