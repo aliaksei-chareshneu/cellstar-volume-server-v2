268a26fdc80803ee366cda43c8e367e48ed5bd84
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-04-25 11:29:05 +0300

    pipeline for building db from json, tested on a single entry (emd-1832)

--- preprocessor/cellstar_preprocessor/tools/deploy_db/build.py
+++ preprocessor/cellstar_preprocessor/tools/deploy_db/build.py
@@ -2,6 +2,7 @@
 
 
 import argparse
+import asyncio
 import atexit
 import multiprocessing
 import os
@@ -10,9 +11,11 @@ import subprocess
 from pathlib import Path
 from typing import Optional
 
-from cellstar_preprocessor.flows.constants import CSV_WITH_ENTRY_IDS_FILE, DEFAULT_DB_PATH, RAW_INPUT_FILES_DIR, TEMP_ZARR_HIERARCHY_STORAGE_PATH
-from cellstar_preprocessor.tools.deploy_db.deploy_process_helper import clean_up_processes, clean_up_raw_input_files_dir, clean_up_temp_zarr_hierarchy_storage
-from cellstar_preprocessor.tools.prepare_input_for_preprocessor.prepare_input_for_preprocessor import csv_to_config_list_of_dicts, prepare_input_for_preprocessor
+from cellstar_db.models import InputForBuildingDatabase
+from cellstar_preprocessor.flows.constants import CSV_WITH_ENTRY_IDS_FILE, DB_BUILDING_PARAMETERS_JSON, DEFAULT_DB_PATH, TEMP_ZARR_HIERARCHY_STORAGE_PATH
+from cellstar_preprocessor.preprocess import PreprocessorMode, main_preprocessor
+from cellstar_preprocessor.tools.deploy_db.deploy_process_helper import clean_up_processes
+from cellstar_preprocessor.tools.prepare_input_for_preprocessor.prepare_input_for_preprocessor import csv_to_config_list_of_dicts, json_to_list_of_inputs_for_building, prepare_input_for_preprocessor
 # from preprocessor_old.main import remove_temp_zarr_hierarchy_storage_folder
 # from preprocessor_old.src.preprocessors.implementations.sff.preprocessor.constants import CSV_WITH_ENTRY_IDS_FILE, DB_NAME_FOR_OME_TIFF, DEFAULT_DB_PATH, RAW_INPUT_FILES_DIR, TEMP_ZARR_HIERARCHY_STORAGE_PATH
 # from preprocessor_old.src.tools.deploy_db.deploy_process_helper import clean_up_processes, clean_up_raw_input_files_dir, clean_up_temp_zarr_hierarchy_storage
@@ -23,50 +26,95 @@ PROCESS_IDS_LIST = []
 
 def parse_script_args():
     parser=argparse.ArgumentParser()
-    parser.add_argument('--csv_with_entry_ids', type=Path, default=CSV_WITH_ENTRY_IDS_FILE, help='csv with entry ids and info for preprocessor')
-    parser.add_argument('--raw_input_files_dir', type=Path, default=RAW_INPUT_FILES_DIR, help='dir with raw input files')
-    parser.add_argument("--db_path", type=Path, default=DEFAULT_DB_PATH, help='path to db folder')
-    parser.add_argument("--temp_zarr_hierarchy_storage_path", type=Path, help='path to db working directory')
+    parser.add_argument('--db_building_parameters_json', type=Path, default=DB_BUILDING_PARAMETERS_JSON, help='')
+    # parser.add_argument('--raw_input_files_dir', type=Path, default=RAW_INPUT_FILES_DIR, help='dir with raw input files')
+    parser.add_argument("--db_path", type=str, default=DEFAULT_DB_PATH, help='path to db folder')
+    parser.add_argument("--temp_zarr_hierarchy_storage_path", type=str, default=TEMP_ZARR_HIERARCHY_STORAGE_PATH, help='path to db working directory')
 
     args=parser.parse_args()
     return args
 
-def _preprocessor_internal_wrapper(entry: dict):
-    lst = [
-        "python", "preprocessor/main.py",
-        "--db_path", entry['db_path'],    
-        "--single_entry", entry['single_entry'],
-        "--entry_id", entry['entry_id'],
-        "--source_db", entry['source_db'],
-        "--source_db_id", entry['source_db_id'],
-        "--source_db_name", entry['source_db_name']
-    ]
-
-    if entry['force_volume_dtype']:
-        lst.extend(['--force_volume_dtype', entry['force_volume_dtype']])
+# common arguments
+def _preprocessor_internal_wrapper(input: InputForBuildingDatabase, db_path: str, working_folder: str):
+    # TODO: run as function
+    # main_preprocessor
+    # before that convert some arguments types used in main_preprocessor
+    quantize_downsampling_levels = None
+    if input['quantize_downsampling_levels']:
+        quantize_downsampling_levels = []
+        quantize_downsampling_levels = " ".join(str(item) for item in input['quantize_downsampling_levels'])
     
-    if entry['quantization_dtype']:
-        lst.extend(['--quantize_volume_data_dtype_str', entry['quantization_dtype']])
+    # there is a list
+    # each item is tuple
+    # need to get two lists
+    inputs = input['inputs']
+    input_pathes_list = [Path(i[0]) for i in inputs]
+    input_kinds_list = [i[1] for i in inputs]
+    # TODO: use starmap?
+    asyncio.run(
+        main_preprocessor(
+            mode=PreprocessorMode.add,
+            quantize_dtype_str=input['quantize_dtype_str'],
+            quantize_downsampling_levels=quantize_downsampling_levels,
+            force_volume_dtype=input['force_volume_dtype'],
+            max_size_per_downsampling_lvl_mb=input['max_size_per_downsampling_lvl_mb'],
+            min_size_per_downsampling_lvl_mb=input['min_size_per_downsampling_lvl_mb'],
+            max_downsampling_level=input['max_downsampling_level'],
+            min_downsampling_level=input['min_downsampling_level'],
+            remove_original_resolution=input['remove_original_resolution'],
+            entry_id=input['entry_id'],
+            source_db=input['source_db'],
+            source_db_id=input['source_db_id'],
+            source_db_name=input['source_db_name'],
+            working_folder=Path(working_folder),
+            db_path=Path(db_path),
+            input_paths=input_pathes_list,
+            input_kinds=input_kinds_list,
+        )
+    )
+    
+    
+    # lst = [
+    #     "python", "preprocessor/main.py",
+    #     "--db_path", input,    
+    #     # "--single_entry", entry['single_entry'],
+    #     "--entry_id", entry['entry_id'],
+    #     "--source_db", entry['source_db'],
+    #     "--source_db_id", entry['source_db_id'],
+    #     "--source_db_name", entry['source_db_name']
+    # ]
 
-    if entry['temp_zarr_hierarchy_storage_path']:
-        lst.extend(['--temp_zarr_hierarchy_storage_path', entry['temp_zarr_hierarchy_storage_path']])
+    # if entry['force_volume_dtype']:
+    #     lst.extend(['--force_volume_dtype', entry['force_volume_dtype']])
+    
+    # if entry['quantization_dtype']:
+    #     lst.extend(['--quantize_volume_data_dtype_str', entry['quantization_dtype']])
 
-    if entry['source_db'] == 'idr':
-        lst.extend(['--ome_zarr_path', entry['ome_zarr_path']])
+    # if entry['temp_zarr_hierarchy_storage_path']:
+    #     lst.extend(['--temp_zarr_hierarchy_storage_path', entry['temp_zarr_hierarchy_storage_path']])
 
-    if entry['source_db'] == DB_NAME_FOR_OME_TIFF:
-        lst.extend(['--ome_tiff_path', entry['ome_tiff_path']])
+    # if entry['source_db'] == 'idr':
+    #     lst.extend(['--ome_zarr_path', entry['ome_zarr_path']])
+
+    # if entry['source_db'] == DB_NAME_FOR_OME_TIFF:
+    #     lst.extend(['--ome_tiff_path', entry['ome_tiff_path']])
 
 
-    process = subprocess.Popen(lst)
-    global PROCESS_IDS_LIST
-    PROCESS_IDS_LIST.append(process.pid)
+    # TODO: can run as function instead?
+    # process = subprocess.Popen(lst)
+    # global PROCESS_IDS_LIST
+    # PROCESS_IDS_LIST.append(process.pid)
 
-    return process.communicate()
+    # return process.communicate()
 
-def _preprocessor_external_wrapper(config: list[dict]):
+def _preprocessor_external_wrapper(arguments_list: list[tuple[InputForBuildingDatabase, str, str]]):
+    # need to provide that input:
+    # input: InputForBuildingDatabase, db_path: str, working_folder: str
+    # as list to starmap
+    # arguments_list: list[tuple[InputForBuildingDatabase, str, str]] = []
     with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
-        result_iterator = p.map(_preprocessor_internal_wrapper, config)
+        # TODO: use starmap?
+        result_iterator = p.starmap(_preprocessor_internal_wrapper, arguments_list)
         # print(123)
     
     p.join()
@@ -74,32 +122,40 @@ def _preprocessor_external_wrapper(config: list[dict]):
 def build(args):
     if not args.temp_zarr_hierarchy_storage_path:
         
-        temp_zarr_hierarchy_storage_path = TEMP_ZARR_HIERARCHY_STORAGE_PATH / args.db_path.name
+        temp_zarr_hierarchy_storage_path = Path(TEMP_ZARR_HIERARCHY_STORAGE_PATH) / args.db_path
     else:
-        temp_zarr_hierarchy_storage_path = args.temp_zarr_hierarchy_storage_path
+        temp_zarr_hierarchy_storage_path = Path(args.temp_zarr_hierarchy_storage_path)
 
-    atexit.register(clean_up_temp_zarr_hierarchy_storage, temp_zarr_hierarchy_storage_path)
+    # atexit.register(clean_up_temp_zarr_hierarchy_storage, temp_zarr_hierarchy_storage_path)
 
     # here it is removed
     if temp_zarr_hierarchy_storage_path.exists():
         # remove_temp_zarr_hierarchy_storage_folder(temp_zarr_hierarchy_storage_path)
         shutil.rmtree(temp_zarr_hierarchy_storage_path, ignore_errors=True)
     
-    clean_up_raw_input_files_dir(args.raw_input_files_dir)
+    # clean_up_raw_input_files_dir(args.raw_input_files_dir)
 
-    config = csv_to_config_list_of_dicts(args.csv_with_entry_ids)
-    print('CSV was parsed')
-    updated_config = prepare_input_for_preprocessor(config=config, output_dir=args.raw_input_files_dir,
+    # NOTE: this function should parse JSON to list[tuple[InputForBuildingDatabase]
+    config = json_to_list_of_inputs_for_building(args.db_building_parameters_json)
+    print('JSON was parsed')
+    
+    # this function should create arguments list
+    # arguments_list: list[tuple[InputForBuildingDatabase, str, str]]
+    # from parsed list of InputForBuildingDatabase
+    # and args.db_path and args temp_zarr_hierarchy_storage_path
+    arguments_list = prepare_input_for_preprocessor(config=config,
         db_path=args.db_path,
         temp_zarr_hierarchy_storage_path=temp_zarr_hierarchy_storage_path)
-    print('Input files have been downloaded')
-    _preprocessor_external_wrapper(updated_config)
+    
+    
+    # print('Input files have been downloaded')
+    _preprocessor_external_wrapper(arguments_list)
 
     # TODO: this should be done only after everything is build
     shutil.rmtree(temp_zarr_hierarchy_storage_path, ignore_errors=True)
 
 if __name__ == '__main__':
-    print("DEFAULT PORTS ARE TEMPORARILY SET TO 4000 and 8000, CHANGE THIS AFTERWARDS")
+    # print("DEFAULT PORTS ARE TEMPORARILY SET TO 4000 and 8000, CHANGE THIS AFTERWARDS")
     atexit.register(clean_up_processes, PROCESS_IDS_LIST)
     args = parse_script_args()
     build(args)
---
+++ preprocessor/cellstar_preprocessor/tools/prepare_input_for_preprocessor/prepare_input_for_preprocessor.py
@@ -0,0 +1,193 @@
+
+# e.g. emd-1832 or emd_1832
+import argparse
+import json
+from pathlib import Path
+import re
+import urllib.request
+import os
+import gzip
+import shutil
+import csv
+from cellstar_db.models import InputForBuildingDatabase
+import pandas as pd
+import numpy as np
+
+from preprocessor_old.src.preprocessors.implementations.sff.preprocessor.constants import APPLICATION_SPECIFIC_SEGMENTATION_EXTENSIONS, CSV_WITH_ENTRY_IDS_FILE, DB_NAME_FOR_OME_TIFF, DEFAULT_DB_PATH
+
+# TODO: check if it works with abs path (starting with /)
+# TODO: changed based on Lukas response
+STATIC_INPUT_FILES_DIR = Path('temp/v2_temp_static_entry_files_dir')
+
+TEST_RAW_INPUT_FILES_DIR = Path('temp/test_raw_input_files_dir')
+
+def json_to_list_of_inputs_for_building(json_path: Path):
+    with open(json_path.resolve(), "r", encoding="utf-8") as f:
+            # reads into list
+        read_json: list[InputForBuildingDatabase] = json.load(f)
+    return read_json
+
+def csv_to_config_list_of_dicts(csv_file_path: Path) -> list[dict]:
+    df = pd.read_csv(
+        str(csv_file_path.resolve()),
+        converters={
+            'static_input_files': lambda x: bool(int(x))
+            }
+        )
+
+    df = df.replace({np.nan: None})
+    df['entry_id'] = df['entry_id'].str.lower()
+    list_of_dicts = df.to_dict('records')
+
+    return list_of_dicts
+
+    
+
+def prepare_input_for_preprocessor(config: list[InputForBuildingDatabase], db_path: str,
+    temp_zarr_hierarchy_storage_path: str) -> list[dict]:
+    arguments_list = []
+    for input in config:
+        arguments_list.append(
+            (input, Path(db_path), Path(temp_zarr_hierarchy_storage_path))
+        )
+    print('arguments_list')
+    print(arguments_list)
+    return arguments_list
+    # for entry in config:
+    #     entry['db_path'] = str(db_path.resolve())
+    #     entry['temp_zarr_hierarchy_storage_path'] = str(temp_zarr_hierarchy_storage_path.resolve())
+
+    #     db = re.split('-|_', entry['entry_id'])[0].lower()
+    #     id = re.split('-|_', entry['entry_id'])[-1]
+        
+    #     emdb_folder_name = db.upper() + '-' + id
+    #     emdb_map_gz_file_name = db.lower() + '_' + id + '.map.gz'
+    #     # https://www.ebi.ac.uk/em_static/emdb_sff/10/1014/emd_1014.hff.gz
+    #     volume_browser_gz_file_name = db.lower() + '_' + id + '.hff.gz'
+    #     preprocessor_folder_name = db.lower() + '-' + id
+
+    #     # add to json object addtional info required on preprocessing step
+    #     if db == 'emd':
+    #         entry['source_db'] = 'emdb'
+    #     elif db == 'empiar':
+    #         entry['source_db'] = 'empiar'
+    #     elif db == 'idr':
+    #         entry['source_db'] = 'idr'
+    #     elif db == DB_NAME_FOR_OME_TIFF:
+    #         entry['source_db'] = DB_NAME_FOR_OME_TIFF
+    #     else:
+    #         raise ValueError(f'Source db is not recognized: {db}')
+
+    #     entry_folder = output_dir / entry['source_db'] / preprocessor_folder_name
+    #     entry_folder.mkdir(parents=True, exist_ok=True)
+    #     entry['single_entry'] = str(entry_folder.resolve())
+
+    #     if entry['static_input_files'] and entry['source_db'] not in ['idr', DB_NAME_FOR_OME_TIFF]:
+    #         static_segmentation_file_path = None
+    #         static_folder_content = sorted((STATIC_INPUT_FILES_DIR / entry['source_db'] / preprocessor_folder_name).glob('*'))
+    #         for item in static_folder_content:
+    #             if item.is_file():
+    #                 if item.suffix == '.hff' or item.suffix in APPLICATION_SPECIFIC_SEGMENTATION_EXTENSIONS:
+    #                     static_segmentation_file_path = item
+    #                     # TODO: add segmentation specific exts
+    #                 elif item.suffix == '.map' or item.suffix == '.ccp4' or item.suffix == '.mrc':
+    #                     static_volume_file_path: Path = item
+
+    #         static_map_output_path = entry_folder / static_volume_file_path.name
+    #         shutil.copy2(static_volume_file_path, static_map_output_path)
+
+    #         if static_segmentation_file_path:
+    #             static_sff_output_path = entry_folder / static_segmentation_file_path.name
+    #             shutil.copy2(static_segmentation_file_path, static_sff_output_path)
+
+    #     elif entry['static_input_files'] and entry['source_db'] == 'idr':
+    #         # NOTE: for ome 
+    #         static_ome_zarr_dir_path = None
+    #         static_folder_content = sorted((STATIC_INPUT_FILES_DIR / entry['source_db'] / preprocessor_folder_name).glob('*'))
+    #         for item in static_folder_content:
+    #             if item.is_dir() and item.name.split('.')[1] == 'zarr':
+    #                 static_ome_zarr_dir_path = item
+    #                 static_ome_zarr_dir_output_path = entry_folder / static_ome_zarr_dir_path.name
+    #                 entry['ome_zarr_path'] = static_ome_zarr_dir_output_path
+            
+    #         if not static_ome_zarr_dir_path:
+    #             raise Exception('No ome zarr found')
+
+    #         shutil.copytree(static_ome_zarr_dir_path, static_ome_zarr_dir_output_path)
+    #     elif entry['static_input_files'] and entry['source_db'] == DB_NAME_FOR_OME_TIFF:
+    #         static_ome_tiff_path = None
+    #         static_folder_content = sorted((STATIC_INPUT_FILES_DIR / entry['source_db'] / preprocessor_folder_name).glob('*'))
+    #         for item in static_folder_content:
+    #             if item.is_file() and item.name.split('.')[-1] in ['tif', 'tiff']:
+    #                 static_ome_tiff_path = item
+    #                 static_ome_tiff_output_path = entry_folder / static_ome_tiff_path.name
+    #                 entry['ome_tiff_path'] = static_ome_tiff_output_path
+            
+    #         if not static_ome_tiff_path:
+    #             raise Exception('No ome tiff found')
+
+    #         shutil.copy2(static_ome_tiff_path, static_ome_tiff_output_path)
+    #     else:
+    #         if db == 'emd':
+    #             # Get map
+    #             map_gz_output_path = entry_folder / emdb_map_gz_file_name
+    #             map_request_output = urllib.request.urlretrieve(
+    #                 f'https://ftp.ebi.ac.uk/pub/databases/emdb/structures/{emdb_folder_name}/map/{emdb_map_gz_file_name}',
+    #                 str(map_gz_output_path.resolve())
+    #             )
+
+    #             with gzip.open(str(map_gz_output_path.resolve()), 'rb') as f_in:
+    #                 with open(str(map_gz_output_path.with_suffix('').resolve()), 'wb') as f_out:
+    #                     shutil.copyfileobj(f_in, f_out)
+    #             map_gz_output_path.unlink()
+
+
+    #             # get sff (.hff)
+    #             sff_gz_output_path = entry_folder / volume_browser_gz_file_name
+
+    #             # first two digits of emd ID?
+    #             if len(id) == 4:
+    #                 emdb_sff_prefix_number = id[0:2]
+    #                 sff_request_output = urllib.request.urlretrieve(
+    #                     f'https://www.ebi.ac.uk/em_static/emdb_sff/{emdb_sff_prefix_number}/{id}/{volume_browser_gz_file_name}',
+    #                     str(sff_gz_output_path.resolve())
+    #                 )
+    #             elif len(id) == 5:
+    #                 emdb_sff_prefix_number_1 = id[0:2]
+    #                 emdb_sff_prefix_number_2 = id[2]
+    #                 sff_request_output = urllib.request.urlretrieve(
+    #                     f'https://www.ebi.ac.uk/em_static/emdb_sff/{emdb_sff_prefix_number_1}/{emdb_sff_prefix_number_2}/{id}/{volume_browser_gz_file_name}',
+    #                     str(sff_gz_output_path.resolve())
+    #                 )
+
+    #             with gzip.open(str(sff_gz_output_path.resolve()), 'rb') as f_in:
+    #                 with open(str(sff_gz_output_path.with_suffix('').resolve()), 'wb') as f_out:
+    #                     shutil.copyfileobj(f_in, f_out)
+    #             sff_gz_output_path.unlink()
+        
+    #     # NOTE: if one of them is not present, use source_db and entry_id
+    #     if not entry['source_db_name'] or not entry['source_db_id']:
+    #         entry['source_db_name'] = entry['source_db']
+    #         entry['source_db_id'] = entry['entry_id']
+
+
+
+            # elif db == 'empiar':
+            #     pass
+        # for sff:
+        # https://www.ebi.ac.uk/em_static/emdb_sff/empiar_10087_c2_tomo02/empiar_10087_c2_tomo02.hff.gz
+        # https://www.ebi.ac.uk/em_static/emdb_sff/empiar_10087_e64_tomo03/empiar_10087_e64_tomo03.hff.gz
+        # https://www.ebi.ac.uk/em_static/emdb_sff/empiar_10070_b3talongmusc20130301/empiar_10070_b3talongmusc20130301.hff.gz
+
+        # for maps
+        # any API?
+
+        # 1. 2 empiar VB entries with same ID
+
+        # updated config list of dicts
+    return config
+        
+
+if __name__ == '__main__':
+    config = csv_to_config_list_of_dicts(CSV_WITH_ENTRY_IDS_FILE)
+    updated_config = prepare_input_for_preprocessor(config=config, output_dir=TEST_RAW_INPUT_FILES_DIR, db_path=DEFAULT_DB_PATH, temp_zarr_hierarchy_storage_path=Path(r'dummy_temp_zarr_hierarchy_storage'))
