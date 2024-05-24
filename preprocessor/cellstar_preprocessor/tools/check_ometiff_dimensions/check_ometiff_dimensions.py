fe8e51b90c1d291553ae2619bb123cb4ad130bf3
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-05-24 10:56:38 +0200

    check ometiff dimensions script

---
+++ preprocessor/cellstar_preprocessor/tools/check_ometiff_dimensions/check_ometiff_dimensions.py
@@ -0,0 +1,17 @@
+from pyometiff import OMETIFFReader
+from pathlib import Path
+
+pathes: list[Path] = [
+    Path('test-data/preprocessor/sample_volumes/custom/custom-allencell_230741/allencell_230741_volume.ome.tif'),
+    Path('test-data/preprocessor/sample_segmentations/custom/custom-allencell_230741/allencell_230741_segmentation.ome.tif')
+]
+
+for p in pathes:
+    print(f'Opening {p.name}')
+    reader = OMETIFFReader(fpath=p)
+    img_array, metadata, xml_metadata = reader.read()
+    
+
+    print(p.name)
+    print('Dimension order: ', metadata['DimOrder BF Array'])
+    print('Data array shape', img_array.shape)
